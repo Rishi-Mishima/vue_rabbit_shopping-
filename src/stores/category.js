6077df523b18d471be6927f863d4613ab9878ca1
Rishi-Mishima <153814332+Rishi-Mishima@users.noreply.github.com>
2025-10-30 13:25:16 +0100

    pinia管理header 和 Fixed

---
+++ src/stores/category.js
@@ -0,0 +1,21 @@
+import { ref, } from 'vue'
+import { defineStore } from 'pinia'
+import { getCategoryAPI } from '@/apis/layout'
+
+export const useCategoryStore = defineStore('category', () => {
+    // 导航列表的逻辑
+    // state 
+    const categoryList = ref([])
+
+    // action 获取导航数据的方法
+    const getCategory = async () => {
+        const res = await getCategoryAPI()
+        console.log(res);
+        categoryList.value = res.data.result
+    }
+
+    return {
+        categoryList,
+        getCategory
+    }
+})
