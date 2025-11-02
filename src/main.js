d4e14bb0f288324a58ae7ee8fa41c900149d81b2
Rishi-Mishima <153814332+Rishi-Mishima@users.noreply.github.com>
2025-11-02 18:35:09 +0100

    实现图片懒加载

--- src/main.js
+++ src/main.js
@@ -8,7 +8,8 @@ import router from './router'
 
 import '@/styles/common.scss'
 
-
+import { useIntersectionObserver } from '@vueuse/core'
+//import { shallowRef, useTemplateRef } from 'vue'
 
 const app = createApp(App)
 
@@ -16,3 +17,16 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+// 全局指令
+app.directive('img-lazy', {
+    mounted(el, binding) {
+        const { stop } = useIntersectionObserver(el, ([{ isIntersecting }]) => {
+            console.log(isIntersecting)
+            if (isIntersecting) { // 进入视口区域 
+                el.src = binding.value
+                stop()
+            }
+        },)
+    }
+})
