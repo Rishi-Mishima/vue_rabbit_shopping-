bba82fa3c802995f78c2b88d029e01df1cb8601d
Rishi-Mishima <153814332+Rishi-Mishima@users.noreply.github.com>
2025-11-06 15:46:00 +0100

    组件props GoodsItem 封装

--- src/views/Home/components/HomeProduct.vue
+++ src/views/Home/components/HomeProduct.vue
@@ -2,6 +2,9 @@
 import HomePanel from './HomePanel.vue'
 import { getGoodsAPI } from '@/apis/home'
 import { ref, onMounted} from 'vue'
+
+import GoodsItem from './GoodsItem.vue'
+
 const goodsProduct = ref([])
 const getGoods = async () => {
   const res = await getGoodsAPI()
@@ -25,13 +28,15 @@ onMounted( ()=> getGoods() )
         </RouterLink>
         <ul class="goods-list">
           <li v-for="goods in cate.goods" :key="goods.id">
-            <RouterLink to="/" class="goods-item">
+            <!-- <RouterLink to="/" class="goods-item">
               <img v-img-lazy="goods.picture" alt="" />
               <p class="name ellipsis">{{ goods.name }}</p>
               <p class="desc ellipsis">{{ goods.desc }}</p>
               <p class="price">&yen;{{ goods.price }}</p>
-            </RouterLink>
+            </RouterLink> -->
+            <GoodsItem :goods="goods"></GoodsItem>
           </li>
+
         </ul>
       </div>
     </HomePanel>
@@ -125,41 +130,7 @@ onMounted( ()=> getGoods() )
       }
     }
 
-    .goods-item {
-      display: block;
-      width: 220px;
-      padding: 20px 30px;
-      text-align: center;
-      transition: all .5s;
 
-      &:hover {
-        transform: translate3d(0, -3px, 0);
-        box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
-      }
-
-      img {
-        width: 160px;
-        height: 160px;
-      }
-
-      p {
-        padding-top: 10px;
-      }
-
-      .name {
-        font-size: 16px;
-      }
-
-      .desc {
-        color: #999;
-        height: 29px;
-      }
-
-      .price {
-        color: $priceColor;
-        font-size: 20px;
-      }
-    }
   }
 }
 </style>
