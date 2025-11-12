c5264c2523f0c7b2877d5342b38f1c3ad695eaa5
Rishi-Mishima <153814332+Rishi-Mishima@users.noreply.github.com>
2025-11-12 18:38:07 +0800

    登陆表单校验-同意协议-自定义规则

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -8,7 +8,8 @@ import { triggerRef } from 'vue';
 
 const form = ref({
   account : '',
-  password : ''
+  password : '',
+  agree:true
 })
 
 //2. 规则对象
@@ -20,6 +21,19 @@ const rules={
     {required: true, message: '密码不能为空', trigger:'blur'},
     {
       min:6, max:14, message: '密码需为6 - 14位', trigger:'blur'
+    }
+  ],
+  //同意协议验证
+  agree:[
+    {
+      validator:(rule,value,callback) =>{
+        console.log(value);
+        if(value){
+          callback()
+        }else{
+          callback(new Error('请勾选协议'))
+        }
+      }
     }
   ]
 }
@@ -56,8 +70,8 @@ const rules={
               <el-form-item prop="password" label="密码">
                 <el-input v-model="form.password"/>
               </el-form-item>
-              <el-form-item label-width="22px">
-                <el-checkbox  size="large">
+              <el-form-item prop="agree" label-width="22px">
+                <el-checkbox v-model="form.agree" size="large">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
