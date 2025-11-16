7be7e3056546b577263297e9b76f446738872644
Rishi-Mishima <153814332+Rishi-Mishima@users.noreply.github.com>
2025-11-17 01:04:56 +0800

    头部购物车 - 列表删除

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -25,8 +25,17 @@ export const userCartStore = defineStore('cart', () => {
         sessionStorage.setItem('cart-list', JSON.stringify(cartList.value))
     }
 
+    const delCart = async (skuId) => {
+        // 思路：
+        // 1. 找到要删除项的下标值 - splice
+        // 2. 使用数组的过滤方法 - filter
+        const idx = cartList.value.findIndex((item) => skuId === item.skuId)
+        cartList.value.splice(idx, 1)
+    }
+
     return {
         cartList,
-        addCart
+        addCart,
+        delCart
     }
 })
