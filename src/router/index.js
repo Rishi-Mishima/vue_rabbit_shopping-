a7f8e611cfadcc0285a5629492813e865a0cb4c3
Rishi-Mishima <153814332+Rishi-Mishima@users.noreply.github.com>
2025-11-21 12:01:21 +0800

    下单结算路由跳转

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,9 @@ import Detail from '@/views/Detail/index.vue'
 
 import CartList from '@/views/CartList/index.vue'
 
+import Checkout from '@/views/Checkout/index.vue'
+
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -36,6 +39,10 @@ const router = createRouter({
         {
           path: 'cartlist',
           component: CartList
+        },
+        {
+          path: 'checkout',
+          component: Checkout
         }
       ]
     },
